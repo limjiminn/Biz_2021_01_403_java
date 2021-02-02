87eea2f1d2d484de8c6858c634cc207d4bd895c1
limjiminn <jiaezzan@naver.com>
2021-02-02 15:31:33 +0900

    HomeWork 추가

---
+++ Java_003/src/com/callor/hello/Homework.java
@@ -0,0 +1,11 @@
+package com.callor.hello;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HomeWork");
+
+	}
+
+}
