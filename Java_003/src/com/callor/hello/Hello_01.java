1b2a4df03778c7eb01415981e70283d1294ad9b1
limjiminn <jiaezzan@naver.com>
2021-02-02 14:12:02 +0900

    명령어

---
+++ Java_003/src/com/callor/hello/Hello_01.java
@@ -0,0 +1,18 @@
+package com.callor.hello;
+
+public class Hello_01 {
+
+	public static void main(String args[]) {
+		
+		System.out.println("여기는 Hello_01 클래스");  //숫자 0
+		System.out.println("여기는 Hello_O1 클래스");  // 대문자 O
+		System.out.println();
+		
+		System.out.print("30 + 40 = ");
+		System.out.println(30 + 40);
+		
+		System.out.print("Rupublic ");
+		System.out.print("of ");
+		System.out.println("Korea");
+	}
+}
