3991f54839297aa031916e8dc7578c699e0a04a4
limjiminn <jiaezzan@naver.com>
2021-02-04 16:11:27 +0900

    반복문 학습

--- Java_006/src/com/callor/varriable/Loop_04.java
+++ Java_006/src/com/callor/varriable/Loop_04.java
@@ -11,7 +11,7 @@ public class Loop_04 {
 		System.out.println("구구단");
 		System.out.println("---------------------------");
 		
-		for(; num1 < 10 ;) {
+		for(num1 = 1; num1 < 10 ;) {
 			
 			System.out.print(intDan);
 			System.out.print(" x ");
@@ -19,7 +19,31 @@ public class Loop_04 {
 			System.out.print(" = ");
 			System.out.println(intDan * num1);
 			// 반복문으로 코드를 묶는다.
-			num1  += 1;
+			num1 += 1;
+		}
+		for(num1 = 1; num1 < 5;) {
+			System.out.println(num1);
+			num1 += 1;
+		}
+		System.out.println("============================");
+		/*
+		 * for() 명령문이 실행되는 순서
+		 * 최초에 한번 (1) 실행해서 num1을 1로 세팅
+		 * (2) 실행해서 num1 < 10 물어보기
+		 * 결과가 "예"이면 (3)~(7)순서대로 실행
+		 * (8) 실행해서 num1을 1증가
+		 * 다시 (2)를 실행해서 num1 < 10 물어보기
+		 * 
+		 * 이렇게 반복
+		 * num1 < 10 이 "아니오" 가 되면 for문 종료.
+		 */
+		//      (1)       (2)       (8)
+		for(num1 = 1; num1 < 10; num1 += 1) {
+			System.out.print(intDan);  //(3)
+			System.out.print(" x ");  //(4)
+			System.out.print(num1);   //(5)
+			System.out.print(" = ");  //(6)
+			System.out.println(intDan * num1); //(7)
 		}
 	}
 }
