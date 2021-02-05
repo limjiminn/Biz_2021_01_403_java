26e64b7b34910bcf802a5b11d1c755a09d39f68b
limjiminn <jiaezzan@naver.com>
2021-02-05 16:44:13 +0900

    while

--- Java_007/src/com/callor/start/loop/While_03.java
+++ Java_007/src/com/callor/start/loop/While_03.java
@@ -6,7 +6,12 @@ public class While_03 {
 		
 		int index = 0;
 		while(true) {
+			// 매우 불편한 코드
+			if(index % 3 > 0) {
+				System.out.println("3의 배수가 아님");
+			}
 			
+			// 정상적인 코드
 			if(++index % 3 == 0) {
 				System.out.println(index + " 는 3의 배수");
 			} else {   //나머지가 1또는 2인경우
@@ -17,7 +22,8 @@ public class While_03 {
 			if(index > 1000) {
 				break;
 			}
-			
+			//if(index == 999){  break; }  :인덱스가 절대적인 정수여야한다.(위험한코드)
+										// : 실수가 있다면 절대 멈추지않는다.
 			
 			
 		}
