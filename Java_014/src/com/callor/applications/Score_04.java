67202f13f9a083fd9e1080554d7889d283bb88ff
limjiminn <jiaezzan@naver.com>
2021-02-22 12:24:16 +0900

    score

--- Java_014/src/com/callor/applications/Score_04.java
+++ Java_014/src/com/callor/applications/Score_04.java
@@ -19,19 +19,17 @@ public class Score_04 {
 
 		for (int i = 0; i < intKor.length; i++) {
 
-			//System.out.print((i + 1) + "번 학생 국어점수 : ");
 			intKor[i] = rnd.nextInt(100) + 1;
 
-			//System.out.print((i + 1) + "번 학생 영어점수 : ");
 			intEng[i] = rnd.nextInt(100) + 1;
 
-			//System.out.print((i + 1) + "번 학생 수학점수 : ");
 			intMath[i] = rnd.nextInt(100) + 1;
-			//System.out.println("");
+
 		}
 		System.out.println("================================");
 		System.out.println("이름\t국어\t영어\t수학");
 		System.out.println("--------------------------------");
+
 		for (int i = 0; i < intKor.length; i++) {
 			System.out.print(strName[i] + "\t");
 			System.out.print(intKor[i] + "\t");
