a850bddc4a99a8f8e9d9cd3345bf9e64fd560d12
limjiminn <jiaezzan@naver.com>
2021-02-23 16:20:19 +0900

    prime

--- Java_015/src/com/callor/applications/Prime_03.java
+++ Java_015/src/com/callor/applications/Prime_03.java
@@ -1,10 +1,25 @@
 package com.callor.applications;
 
+import java.util.Scanner;
+
+import com.callor.applications.service.PrimeServiceV2;
+
 public class Prime_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		PrimeServiceV2 psV2 = new PrimeServiceV2();
+		
+		System.out.print("숫자입력 >> ");
+		int num = scan.nextInt();
+		
+		int result =psV2.prime(num);
+		if(result < 0) {
+			System.out.println(num + "는 소수가 아님");
+		}else {
+			System.out.println(num + "는 소수");
+		}
 	}
 
 }
--- Java_015/src/com/callor/applications/service/PrimeServiceV2.java
+++ Java_015/src/com/callor/applications/service/PrimeServiceV2.java
@@ -1,21 +1,25 @@
 package com.callor.applications.service;
 
 public class PrimeServiceV2 {
-	
+
 	public int prime(int num) {
+		//num 값이 소수이면 return num
+		//아니면 return -1
 		int i = 0;
-		for(i = 2; i < num; i++) {
-			if(num % i == 0) {
-				break;
+
+		for (i = 2; i < num; i++) {
+			
+			//true 이면 소수가 아니다.
+			if (num % i == 0) {
+				//prime() method 실행을 중단하고 
+				//main() method 에게 -1을 되돌려줘라
+				return -1;
 			}
 		}
-		
-		if(i < num) {
-			return num;
-		}else {
-			return -1;
-			
-		}
+		//num 값이 소수이여서 for()반복문을 모두 수행하고
+		// 여기에 다다르면 num값을 그대로 return하라
+		return num;
+
 	}
 
 }
