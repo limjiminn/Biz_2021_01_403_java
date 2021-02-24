cd565f182aff07a712b10a53969413e52c5de469
limjiminn <jiaezzan@naver.com>
2021-02-24 14:44:06 +0900

    array

--- Java_016/src/com/callor/score/Score_02.java
+++ Java_016/src/com/callor/score/Score_02.java
@@ -1,10 +1,60 @@
 package com.callor.score;
 
+import java.util.Random;
+
+import com.callor.score.service.ScoreServiceV1;
+
 public class Score_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String strName[] = {"홍길동","이몽룡","성춘향","장녹수","임꺽정"};
+		int intKor[] = new int[5];
+		int intEng[] = new int[5];
+		int intMath[] = new int[5];
+		
+		int intSum[] = new int[5];
+		float floatAvg[] = new float[5];
+		ScoreServiceV1 ssV1 = new ScoreServiceV1();
+		
+		Random rnd = new Random();
+		
+		
+		
+		for(int i = 0; i < strName.length; i++) {
+			intKor[i] = rnd.nextInt(100) + 1;
+			intEng[i] = rnd.nextInt(100) + 1;
+			intMath[i] = rnd.nextInt(100) + 1;	
+		}
+		//총점 계산
+		//ssV1.scoreSum() method를 호출하여 총점 계산
+		//ssV1.scoreSum() 호출하여 총점
+		// 총점을 계산하려면 3과목의 점수를 전달하고
+		// 총점 결과를 다시 return 받아서 ...해야한다.
+		for(int i = 0; i < strName.length ; i++) {
+			//i 번째 학생의 3과목 점수를 scoreSum() 전달하고
+			//결과를 다시 i번째 학생의 총점에 저장
+			//intSum[i] : i번쨰 학생의 총점
+			int sum = ssV1.scoreSum(intKor[i],intEng[i],intMath[i]);
+			intSum[i] = sum;
+		}
+		for(int i = 0; i < strName.length; i++) {
+			float avg = ssV1.scoreAvg(intSum[i]);
+			floatAvg[i] = avg;
+		}
+		
+		for(int i = 0; i < strName.length; i++) {
+			ssV1.scorePrint(strName[i], intKor[i], intEng, intMath,
+					 intSum, floatAvg );
+		}
+		
+		
+		//이름,국어,영어,수학,총점,평균, 배열을
+		//통쨰로 매개변수로 전달하고
+		//ssV1.print() method에서 리스트를 출력하도록
+		ssV1.Print(strName, intKor, intEng, intMath, intSum, floatAvg);
+		
+		
 	}
 
 }
--- Java_016/src/com/callor/score/service/ScoreServiceV1.java
+++ Java_016/src/com/callor/score/service/ScoreServiceV1.java
@@ -3,37 +3,63 @@ package com.callor.score.service;
 import java.util.Random;
 
 public class ScoreServiceV1 {
-	int sum = 0;
-	float fAvg = 0;
-	int intKor = 0;
-	int intEng = 0;
-	int intMath = 0;
+	
 	Random rnd = new Random();
-
+	/*
+	 * 3과목의 점수를 전달받아서
+	 * 합산을 하고 결과를 return하는 method
+	 */
 	// 총점 계산
-	public int scoreSum() {
+	public int scoreSum(int intKor, int intEng, int intMath) {
 		
-			sum = intKor;
+		int	sum = intKor;
 			sum += intEng;
 			sum += intMath;
 
 		return sum;
 	}
-
+	/*
+	 * 총점 점수를 전달받고
+	 * 과목수(과목수 3개라고 가정) 나누어 return 
+	 */
 	// 평균 계산
-	public float scoreAvg() {
-		
-		
-		fAvg = (float) sum / 3;
+	public float scoreAvg(int intSum) {
+			
+	float fAvg = (float) intSum / 3;
 		return fAvg;
 	}
 
 	// 성적 리스트
-	public int scorePrint() {
-		intKor = rnd.nextInt();
-		intEng = rnd.nextInt();
-		intMath = rnd.nextInt();
+	public void scorePrint(String strName,
+			int intKor, int intEng, int intMath,
+			int intSum, float floatAvg) {
+		
+		System.out.printf("%s\t%d\t%d\t%d\t%d\t%3.2f\n",
+				strName, intKor, intEng, intMath,
+				intSum, floatAvg);
+		
+	}
+	/*
+	 * 배열을 매개변수로 받는 변수
+	 */
+	public void print(String[] strName,
+			int[] intKor,int[] intEng,int[] intMath,
+			int[] intSum,float[] floatAvg) {
+		System.out.println("========================");
+		System.out.println("이름\t국어\t영어\t수학\t총점\t평균");
+		System.out.println("-------------------------");
+		for(int i = 0; i <strName.length; i++) {
+			System.out.printf("%s\t%d\t%d\t%d\t%d\t%3.2f\n",
+					strName[i],
+					intKor[i],
+					intEng[i],
+					intMath[i],
+					intSum[i],
+					floatAvg[i]);
+		}
 		
 	}
 
 }
+
+
