8b286944f1c33a9f60f5ca5d6a283c8ad8bb5114
limjiminn <jiaezzan@naver.com>
2021-03-02 12:22:45 +0900

    implement

--- Java_019_interface/src/com/callor/oop/model/CartVO.java
+++ Java_019_interface/src/com/callor/oop/model/CartVO.java
@@ -13,10 +13,66 @@ public class CartVO {
 	private String standard;
 	private String date;
 	private String time;
-	private String qty;
-	private String price;
-	private String total;
+	
+	private int qty;
+	private int price;
+	private int total;
 	
 	
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	@Override
+	public String toString() {
+		return "CartVO [UserName=" + UserName + ", productName=" + productName + ", standard=" + standard + ", date="
+				+ date + ", time=" + time + ", qty=" + qty + ", price=" + price + ", total=" + total + "]";
+	}
 
+	
+	
 }
